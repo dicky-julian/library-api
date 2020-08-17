4d19cbaa953cc9d5c6296d4fd823f83e41b682ce
Dicky Julian <julian18.ech@gmail.com>
2020-08-17 07:05:55 +0700

    add production

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.get('/', (req, res) => res.send('<h2> Hello World! </h2>'));
+router.get('/', (req, res) => res.send('<h2> Hello Deployment 1! </h2>'));
 router.post('/login', authController.loginC);
 router.post('/register', authController.registerC);
 
