c169c75942431acaed83e93e6db6a6d818a08d69
Dicky Julian <julian18.ech@gmail.com>
2020-08-17 07:32:15 +0700

    add production

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
 const db = require('./src/helpers/mysql');
 const routes = require('./src/routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors({
+//     origin: 'https://librarian-apps.web.app/'
+// }));
+app.use(cors());
 app.use('/', routes);
 
 db.connect((err) => {
