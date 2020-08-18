29e84dee4fbe78e2e7ffa34e64b937500ef40780
Dicky Julian <julian18.ech@gmail.com>
2020-08-18 07:45:57 +0700

    add pm2

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.get('/', (req, res) => res.send('<h2> Hello Deployment 2! </h2>'));
+router.get('/', (req, res) => res.send('<h2> Hello Deployment 3! </h2>'));
 router.post('/login', authController.loginC);
 router.post('/register', authController.registerC);
 
